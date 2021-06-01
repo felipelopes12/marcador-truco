6c167be8695e00efb44ec2f105f064c8d8a51d13
felipelopes12 <felipe301016@hotmail.com>
2021-06-01 10:04:26 -0300

    Marcador de truco finalizado

---
+++ README.markdown
@@ -0,0 +1,5 @@
+# Marcador de Truco
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/felipelopes12/pen/eYgdLyX](https://codepen.io/felipelopes12/pen/eYgdLyX).
+
+
---
+++ src/script.js
@@ -0,0 +1,62 @@
+
+var primeiraDupla = {
+nome: "Jogador 1",
+Truco: 0,
+Pontinho: 0,
+Pontuaçao:0
+}
+var segundaDupla = {
+	nome: "Jogador 2",
+	Truco: 0,
+  Pontinho: 0,
+	Pontuaçao:0
+}
+
+primeiraDupla.Pontuaçao = calculaPontos(primeiraDupla)
+segundaDupla.Pontuaçao = calculaPontos(segundaDupla)
+
+function calculaPontos(jogador){
+var Pontuaçao = (jogador.Truco * 3) + jogador.Pontinho
+return Pontuaçao
+} 
+
+var jogadores = [ primeiraDupla, segundaDupla]
+
+exibirJogadoresNaTela(jogadores)
+
+function exibirJogadoresNaTela(jogadores){
+	var html = ""
+	for(var i=0; i < jogadores.length; i++){
+		html += "<tr><td>" + jogadores[i].nome + "</td>"
+		html += "<td>" + jogadores[i].Truco + "</td>"
+	  html += "<td>" + jogadores[i].Pontinho + "</td>"
+		html += "<td>" + jogadores[i].Pontuaçao + "</td></tr>"
+		html += "<td><button onClick='adicionarTruco(" + i + " )'>Truco</button></td>"
+    html += "<td><button onClick='adicionarPontinho(" + i + ")'>Pontinho</button></td>"
+  html += "<td><button onClick='limpar(" + i + ")'>Limpar</button></td></tr>"
+}
+ 	var tabelaJogadores = document.getElementById("tabelaJogadores")
+ 	tabelaJogadores.innerHTML = html
+}
+
+function limpar(i){
+    jogadores[i].Truco = 0
+    jogadores[i].Pontinho = 0
+    jogadores[i].Pontuaçao = 0
+   exibirJogadoresNaTela(jogadores)
+}
+
+function adicionarTruco(i){
+	var jogador = jogadores[i]
+	jogador.Truco++
+	jogador.Pontuaçao = calculaPontos(jogador)
+	exibirJogadoresNaTela(jogadores)
+}
+function adicionarPontinho(i){
+	var jogador = jogadores[i]
+	jogador.Pontinho++
+	jogador.Pontuaçao = calculaPontos(jogador)
+	exibirJogadoresNaTela(jogadores)
+}
+
+
